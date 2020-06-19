667d2e85d6e92122dd6775b5749e39f73ef1b615
AshinVarghese <ashin.pta@gmail.com>
2020-06-19 11:36:44 +0530

    navigation drawer

--- app/src/main/java/com/example/baby/Dashboard.java
+++ app/src/main/java/com/example/baby/Dashboard.java
@@ -1,18 +1,33 @@
 package com.example.baby;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
 
-public class  Dashboard extends AppCompatActivity implements View.OnClickListener {
+public class  Dashboard extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     private CardView cardViewFeeding,cardViewDiaper,cardViewPumping,cardViewSleep,cardViewMedication,cardViewNotes;
     Button logout,profile;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +42,26 @@ public class  Dashboard extends AppCompatActivity implements View.OnClickListene
         cardViewMedication = (CardView) findViewById(R.id.medication_card);
         cardViewNotes = (CardView) findViewById(R.id.notes_card);
 
-        logout = findViewById(R.id.btn_logout);
-        profile = findViewById(R.id.btn_profile);
+       /* logout = findViewById(R.id.btn_logout);
+        profile = findViewById(R.id.btn_profile);*/
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+        //navigation drawer
+
+
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         //adding click listener to cards
@@ -40,22 +73,31 @@ public class  Dashboard extends AppCompatActivity implements View.OnClickListene
         cardViewNotes.setOnClickListener(this);
 
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        /*logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SessionManagement sessionManagement = new SessionManagement(Dashboard.this);
-                sessionManagement.removeSession();
-                finish();
-            }
-        });
 
-        profile.setOnClickListener(new View.OnClickListener() {
+            }
+        });*/
+
+       /* profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Dashboard.this,SetBabyDetail.class);
                 startActivity(intent);
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -97,5 +139,37 @@ public class  Dashboard extends AppCompatActivity implements View.OnClickListene
             default:break;
 
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        /*Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_Logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);*/
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                Intent intent = new Intent(Dashboard.this,SetBabyDetail.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_Logout:
+                SessionManagement sessionManagement = new SessionManagement(Dashboard.this);
+                sessionManagement.removeSession();
+                finish();
+                break;
+           /* case R.id.nav_login: menu.findItem(R.id.nav_Logout).setVisible(true);
+                menu.findItem(R.id.nav_profile).setVisible(true);
+                menu.findItem(R.id.nav_login).setVisible(false);
+                break;
+            case R.id.nav_Logout: menu.findItem(R.id.nav_Logout).setVisible(false);
+                menu.findItem(R.id.nav_profile).setVisible(false);
+                menu.findItem(R.id.nav_login).setVisible(true);
+                break;*/
+            case R.id.nav_share: Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show(); break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
