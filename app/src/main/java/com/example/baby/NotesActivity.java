c8a3c048a81d3474b23f3cf830f4ed133df7ceab
AshinVarghese <ashin.pta@gmail.com>
2020-06-21 21:23:20 +0530

    bug fix

--- app/src/main/java/com/example/baby/NotesActivity.java
+++ app/src/main/java/com/example/baby/NotesActivity.java
@@ -2,13 +2,108 @@ package com.example.baby;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class NotesActivity extends AppCompatActivity {
+
+    ImageButton buttonBack,buttonSubmit;
+    TimePickerDialog timePickerDialog;
+    EditText editTextTime,editTextDate;
+
+    public String getCurrentDate(){
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.getTime());
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(c.getTime());
+
+        return formattedDate;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        editTextDate = findViewById(R.id.et_date);
+        editTextTime = findViewById(R.id.et_time);
+        buttonBack = findViewById(R.id.cancel_button);
+
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotesActivity.this,Dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        editTextDate.setText(getCurrentDate());
+        final Calendar myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, dayOfMonth);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+            private void updateLabel() {
+                String myFormat = "dd-MMM-yyyy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                editTextDate.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(NotesActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+    public void chooseTime(View view) {
+
+        Calendar calendar = Calendar.getInstance();
+        int hourOfDay=calendar.get(Calendar.HOUR);
+        int minute=calendar.get(Calendar.MINUTE);
+
+        timePickerDialog = new TimePickerDialog(NotesActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                String am_pm;
+                if (hourOfDay<12) {
+                    am_pm = "AM";
+                    editTextTime.setText(hourOfDay + ":" + minute + " " + am_pm);
+                }
+                else if (hourOfDay == 12){
+                    am_pm = "PM";
+                    editTextTime.setText(hourOfDay + ":" + minute + " " + am_pm);
+
+                }
+                else {
+                    am_pm = "PM";
+                    editTextTime.setText(hourOfDay + ":" + minute + " " + am_pm);
+
+                }
+            }
+        },hourOfDay,minute,false);
+        timePickerDialog.show();
     }
 }
